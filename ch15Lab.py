412640a15156ceba397fb40acb6b03df91ea4269
alee <alee@fwparker.org>
2017-03-08 12:50:15 -0600

    grade

--- ch15Lab.py
+++ ch15Lab.py
@@ -1,3 +1,7 @@
+# Made changes to linear search.
+# I like the way you tried to use the function here.
+
+# Your binary search did not work.  Check corrections.  There are a bunch.  One major problem was that you had two separate list for dictionary and you mixed them up.  Another thing was that you did not reset the upper and lower limits on every word.
 '''
 Complete the chapter lab at http://programarcadegames.com/index.php?chapter=lab_spell_check
 '''
@@ -22,20 +26,22 @@ print("---Linear Search---")
 
 '''
 list = []
-def check(file):
+def check(file_name):
+    file = open(file_name)
     for line in file:
         words = split_line(line)
         #text_list.append(words)
         for word in words:
             key = word.upper()
-            for i in range(len(dictionary_list)):
+            for i in range(len(dictionary)):
                 #print(dictionary_list[i])
-                if dictionary_list[i] == key:
+                if dictionary[i] == key:
                     break
             else:
                 print(key, "not found.")
+    file.close()
 
-check(file)
+check("AliceInWonderLand200.txt")
 print()
 '''
 
@@ -44,21 +50,28 @@ print()
 
 print("---Binary Search---")
 file = open("AliceInWonderLand200.txt")
-list = []
 line_number = 0
-upper_bound = len(list) - 1
+upper_bound = len(dictionary) - 1
 lower_bound = 0
 found = False
 for line in file:
+
     line_number += 1
     words = split_line(line)
     for word in words:
         key = word
+        upper_bound = len(dictionary) - 1
+        lower_bound = 0
+        found = False
         while lower_bound <= upper_bound and not found:
             middle_position = (lower_bound + upper_bound) // 2
 
-            if list[middle_position] < key.upper():
+            if dictionary[middle_position] < key.upper():
                 lower_bound = middle_position + 1
+            elif dictionary[middle_position] > key.upper():
+                upper_bound = middle_position - 1
             else:
                 found = True
+        if not found:
+            print(word, "not found")
 file.close()
